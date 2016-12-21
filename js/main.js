945f4694628a02ede3af1d853e959b42f73eb9b7
ycxYI <1359762297@qq.com>
2016-12-21 12:51:39 +0800

    添加menu点击样式

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-(function () {
-    
-})
+$(function(){
+
+    $("#menu li").click(function() {
+
+        $(this).siblings('li').removeClass('current_page_item');  // 删除其他兄弟元素的样式
+
+        $(this).addClass('current_page_item');                     // 添加当前元素的样式
+
+    });
+
+
+
+}); 
+
