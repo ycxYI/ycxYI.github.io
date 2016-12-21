3d38a49ea23839d56b6636f10c1b7c706b0d7d53
ycxYI <1359762297@qq.com>
2016-12-21 17:26:54 +0800

    框架

--- js/main1.js
+++ js/main1.js
@@ -1,13 +1,14 @@
-$(function(){
-
-    $("#menu li").click(function() {
-
+ $(function(){
+    
+    $("#menuul li").click(function() {
+        
         $(this).siblings('li').removeClass('current_page_item');  // 删除其他兄弟元素的样式
 
         $(this).addClass('current_page_item');                     // 添加当前元素的样式
 
     });
 
+ 
 
 
 }); 
