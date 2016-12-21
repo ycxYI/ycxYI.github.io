14aa428756e2358400cb126ac2f33c6783dc3422
ycxYI <1359762297@qq.com>
2016-12-21 22:18:58 +0800

    游戏

--- js/main1.js
+++ js/main1.js
@@ -1,15 +1,18 @@
- $(function(){
-    
-    $("#menuul li").click(function() {
-        
-        $(this).siblings('li').removeClass('current_page_item');  // 删除其他兄弟元素的样式
+ $(function() {
 
-        $(this).addClass('current_page_item');                     // 添加当前元素的样式
+         
+     $("#menuul li").click(function() {
 
-    });
+                 
+         $(this).siblings('li').removeClass('current_page_item');  // 删除其他兄弟元素的样式
 
- 
+                 
+         $(this).addClass('current_page_item');                      // 添加当前元素的样式
+
+             
+     });
+
+      
 
 
-}); 
-
+ }); 
